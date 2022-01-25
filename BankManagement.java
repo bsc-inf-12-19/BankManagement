c0d314fd41df5e2223128c79bd3d6c30b8582eca
bsc-inf-12-19 <bsc-inf-12-19@unima.ac.mw>
2022-01-25 11:36:17 -0800

    adddede very few implementations and features

--- BankManagement.java
+++ BankManagement.java
@@ -6,6 +6,10 @@ public class BankManagement{
         BusinessAccounts b1 = new BusinessAccounts();
         StudentAccounts s1 = new StudentAccounts();
         a1.menu();
+        int number = 1;
+        String name;
+        String branch = "";
+        double balance;
         int num = input.nextInt();
         if(num == 2){
             System.out.println("\nSelect the Bank Account type");
@@ -14,20 +18,20 @@ public class BankManagement{
             System.out.print("\nEnter your choice: ");
             int choice = input.nextInt();
             if(choice == 1){
-                System.out.println("\tCreating a new student Bank Account");
+                System.out.println("\t\nCreating a new student Bank Account");
                 System.out.print("Enter Account Number: ");
-                int number = (int)( 10000 +Math.random() * 90000);
+                number = (int)( 10000 +Math.random() * 90000);
                 System.out.print(number);
                 System.out.print("\nEnter Account Name: ");
-                String name = input.next();
+                name = input.next();
                 s1.setAccountName(name);
                 System.out.print("Enter student Reg #: ");
                 String reg = input.next();
                 s1.setregNumber(reg);
                 System.out.print("Enter Branch: ");
-                String branch = input.next();
+                branch = input.next();
                 System.out.print("Enter Balance: ");
-                double balance = input.nextDouble();
+                balance = input.nextDouble();
                 s1.setBalance(balance);
                 if(s1.getBalance() < 200){
                     System.out.println("\nAmount very small");
@@ -37,8 +41,29 @@ public class BankManagement{
                     System.out.println("\nAccount successfully created.");
                 }
             }
-            System.out.println
-            
+            System.out.println("\nPress Any Key to Continue:");
+            String key = input.next();
+            if(!key.equals("aa")){
+                System.out.println("\n\tSelect operation Below");
+                a1.menu();
+                num = input.nextInt();
+                if(num == 1){
+                    System.out.println("\nAccount Number  Account Name  Type  Branch   Balance");
+                    System.out.println("----------------------------------------------------");
+                    System.out.println(number+        "             "  +s1.getAccountName()+  "   Savings    "   +branch+ "    "   +s1.getBalance());
+                    System.out.println("\nPress Any Key to Continue:");
+                    key = input.next();
+                }
+            }
+            System.out.println("\nPress any key to continue:");
+            if(!key.equals("aa")){
+                System.out.println("\n\tSelect operation Below");
+                a1.menu();
+                num = input.nextInt();
+                if(num == 6){
+                 System.exit(6);   
+                }
+            }
         }
     }
 }
